df7cbde98e7b97bd01bb442982a681458ebec0a9
cristianoliveira <contato@cristianoliveira.com.br>
2024-11-10 17:16:20 +0100

    chore(dx): adds nix develop shell

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+  with pkgs; stdenv.mkDerivation rec {
+    name = "python-virtualenv-shell";
+    env = buildEnv { name = name; paths = buildInputs; };
+    buildInputs = [
+      python3
+      python3Packages.virtualenv
+      # It is essential **not** to add `pip` here as
+      # it would prevent proper virtualenv creation.
+      inotify-tools
+      ulauncher
+      gtk3
+    ];
+    shellHook = ''
+      # set SOURCE_DATE_EPOCH so that we can use python wheels
+      SOURCE_DATE_EPOCH=$(date +%s)
+
+      virtualenv venv
+
+      source venv/bin/activate
+    '';
+  }
